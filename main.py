1d6b592bac5a0ff04bb6aa6863f31aa13cd1895e
Mitesh Gupta <mgmiteshgupta134@gmail.com>
2024-07-04 19:02:31 +0530

    Final Changes

--- main.py
+++ main.py
@@ -17,53 +17,53 @@ vehicle_list=[2,3,5,7]
 
 results={}
 frame_count=-1
-while True:
+success=True
+while success:
     frame_count+=1
+
     # Read Frames
     success,frame=cap.read()
-    if not success or frame_count==10:
-         break
+    if success:
+        results[frame_count]={}
 
-    
-    results[frame_count]={}
+        # Detect Vehicles
+        detections=vehicle_detection_model(frame)[0]
+        detect=[]
+        for detection in detections.boxes.data.tolist():
+            x1,y1,x2,y2,score,class_id=detection
+            if int(class_id) in vehicle_list:
+                detect.append([x1,y1,x2,y2,score])
 
-    # Detect Vehicles
-    detections=vehicle_detection_model(frame)[0]
-    detect=[]
-    for detection in detections.boxes.data.tolist():
-        x1,y1,x2,y2,score,class_id=detection
-        if int(class_id) in vehicle_list:
-            detect.append([x1,y1,x2,y2,score])
+        # Track Vehicles
+        track_ids=vehicle_tracker.update(np.asarray(detect))
 
-    # Track Vehicles
-    track_ids=vehicle_tracker.update(np.asarray(detect))
+        # Detect License Plates
+        license_plates=license_plate_detector(frame)[0]
+        for license_plate in license_plates.boxes.data.tolist():
+                x1,y1,x2,y2,score,class_id=license_plate
 
-    # Detect License Plates
-    license_plates=license_plate_detector(frame)[0]
-    for license_plate in license_plates.boxes.data.tolist():
-            x1,y1,x2,y2,score,class_id=license_plate
+                # Assign License Plate To Car
+                x_car1,y_car1,x_car2,y_car2,car_id=get_car(license_plate,track_ids)
 
-            # Assign License Plate To Car
-            x_car1,y_car1,x_car2,y_car2,car_id=get_car(license_plate,track_ids)
+                if car_id!=-1:
+                    # Crop License Plate
+                    license_plate_crop=frame[int(y1):int(y2), int(x1):int(x2),:]
 
-            # Crop License Plate
-            license_plate_crop=frame[int(y1):int(y2), int(x1):int(x2)]
+                    # Process License PLate
+                    gray_license_plate=cv2.cvtColor(license_plate_crop,cv2.COLOR_BGR2GRAY)
+                    _,threshold_license_plate=cv2.threshold(gray_license_plate,64,255,cv2.THRESH_BINARY_INV)
 
-            # Process License PLate
-            gray_license_plate=cv2.cvtColor(license_plate_crop,cv2.COLOR_BGR2GRAY)
-            _,threshold_license_plate=cv2.threshold(gray_license_plate,64,255,cv2.THRESH_BINARY_INV)
+                    # Read License Plate
+                    license_plate_text,license_plate_text_score=read_license_plate(threshold_license_plate)
 
-            # Read License Plate
-            license_plate_text,license_plate_text_score=read_license_plate(threshold_license_plate)
-
-            if license_plate is not None:
-                results[frame_count][car_id]={'car':{"bbox":[x_car1,y_car1,x_car2,y_car2]},
-                                        'license_plate':{'bbox':[x1,y1,x2,y2],
-                                                        'text':license_plate_text,
-                                                        'bbox_score':score,
-                                                        'text_score':license_plate_text_score}}
+                    if license_plate is not None:
+                        results[frame_count][car_id]={'car':{"bbox":[x_car1,y_car1,x_car2,y_car2]},
+                                                'license_plate':{'bbox':[x1,y1,x2,y2],
+                                                                'text':license_plate_text,
+                                                                'bbox_score':score,
+                                                                'text_score':license_plate_text_score}}
                 
             
 # Write Results
-# write_csv(results,'E:\\Data Science\\Projects\\Automatic-Number-Plate-Recognition\\test.csv')
+write_csv(results,'./test.csv')
 print('CSV Done')
